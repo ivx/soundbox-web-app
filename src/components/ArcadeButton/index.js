92101daf7d3b040ae32a1b6fd67b89a5e683e229
Stefan Schiffer <stefan.schiffer@invision.de>
2017-11-08 11:23:41 +0100

    Clean Up

--- src/components/ArcadeButton/index.js
+++ src/components/ArcadeButton/index.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 
 class ArcadeButton extends Component {
-  handleClick = () => {
-    alert('alaaarrm!');
-  };
-
   render() {
     const classNames = classnames('arcade-button');
 
