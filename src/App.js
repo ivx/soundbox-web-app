f8cb88f4ae276056150c1212a1c63e092173a958
Stefan Schiffer <stefan.schiffer@invision.de>
2017-11-08 11:41:36 +0100

    Add buttons

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ArcadeButton />
+        <ArcadeButton id="btn_0" />
+        <ArcadeButton id="btn_1" />
+        <ArcadeButton id="btn_2" />
+        <ArcadeButton id="btn_3" />
+        <ArcadeButton id="btn_4" />
+        <ArcadeButton id="btn_5" />
+        <ArcadeButton id="btn_6" />
+        <ArcadeButton id="btn_7" />
+        <ArcadeButton id="btn_8" />
+        <ArcadeButton id="btn_9" />
+        <ArcadeButton id="btn_shift" />
       </div>
     );
   }
