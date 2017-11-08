4ce62da2180e77db781df1db53552b40c68178a2
Florian Becker <florian.becker@invision.de>
2017-11-08 23:05:52 +0100

    Have labeled text below buttons

--- src/components/ArcadeButton/index.js
+++ src/components/ArcadeButton/index.js
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 
 class ArcadeButton extends Component {
+  constructor(props) {
+    super(props);
+    this.labelRotation = Math.floor(Math.random() * 12 + -5);
+  }
+
   handleClick = () => {
     this.props.handlePress(this.props.btn_id);
   };
 
   render() {
     const classNames = classnames('arcade-button', {
-      active: this.props.active,
+      active: this.props.active
     });
 
     return (
-      <button
-        className={classNames}
-        onClick={this.handleClick}
-        id={`btn_${this.props.btn_id}`}
-      >
-        {this.props.children}
-      </button>
+      <div className="button-wrapper">
+        <button
+          className={classNames}
+          onClick={this.handleClick}
+          id={`btn_${this.props.btn_id}`}
+        />
+        <div style={{ transform: `rotate(${this.labelRotation}deg)` }}>
+          <span className="button-label">{this.props.children}</span>
+        </div>
+      </div>
     );
   }
 }
