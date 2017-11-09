a45482c0be7f8c78c1b971a0b00d5aa97dd68a34
Florian Becker <florian.becker@invision.de>
2017-11-09 14:27:03 +0100

    Add visual dropzones

--- src/components/ArcadeButton/index.js
+++ src/components/ArcadeButton/index.js
@@ -21,7 +21,7 @@ class ArcadeButton extends Component {
         this.props.handleUpload(
           reader.result.split(',')[1],
           this.props.btn_id,
-          file.name.slice(0, -4),
+          file.name.slice(0, -4)
         );
       };
     }
@@ -29,17 +29,19 @@ class ArcadeButton extends Component {
 
   render() {
     const classNames = classnames('arcade-button', {
-      active: this.props.active,
+      active: this.props.active
     });
 
     return (
       <div className="button-wrapper">
         {this.props.isShift ? (
-          <button
-            className={classNames}
-            onClick={this.handleClick}
-            id={`btn_${this.props.btn_id}`}
-          />
+          <div className="dropzone">
+            <button
+              className={classNames}
+              onClick={this.handleClick}
+              id={`btn_${this.props.btn_id}`}
+            />
+          </div>
         ) : (
           <Dropzone
             disableClick
